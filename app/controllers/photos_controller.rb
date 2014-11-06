3563cc76c39e894a6020ffacd3083dc9a96fb91b
Jimmy <acsd68500@gmail.com>
2014-11-06 17:27:29 +0800

    upload photos done

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,5 +1,40 @@
 class PhotosController < ApplicationController
-  def index
+  before_action :find_id, only: [:show, :edit, :update, :destroy]
 
+  def index
+    @photos = Photo.all
+  end
+
+  def show
+
+  end
+
+  def create
+    @photo = Photo.create(photo_params)
+    if @photo.save
+      redirect_to photos_path
+    end
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def find_id
+    @photo = Photo.find(params[:id])
+  end
+
+  def photo_params
+    params.require(:photo).permit(:avatar)
   end
 end
