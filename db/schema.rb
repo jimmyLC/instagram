c1341114e361a457e46b2a745a54c824de81cf17
Jimmy <acsd68500@gmail.com>
2014-11-07 13:39:12 +0800

    add tog and tagging model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141106145124) do
+ActiveRecord::Schema.define(version: 20141106161707) do
 
   create_table "comments", force: true do |t|
     t.text     "comment"
@@ -34,6 +34,20 @@ ActiveRecord::Schema.define(version: 20141106145124) do
 
   add_index "photos", ["comment_id"], name: "index_photos_on_comment_id"
   add_index "photos", ["user_id"], name: "index_photos_on_user_id"
+
+  create_table "taggings", force: true do |t|
+    t.integer  "photo_id"
+    t.integer  "tag_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "tags", force: true do |t|
+    t.string   "name"
+    t.integer  "count"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email"
