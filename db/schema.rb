e942402b46acbcae6729a280fdc09868647cb2e8
Jimmy <acsd68500@gmail.com>
2014-11-07 19:22:05 +0800

    fix db bug

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
 ActiveRecord::Schema.define(version: 20141107081519) do
-=======
-ActiveRecord::Schema.define(version: 20141106161707) do
->>>>>>> tagging
 
   create_table "comments", force: true do |t|
     t.text     "comment"
