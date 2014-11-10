a738f1ce3b4bba8950454b6edbc21819e328bd7a
Jimmy <acsd68500@gmail.com>
2014-11-10 17:31:23 +0800

    refactor current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,7 @@ class ApplicationController < ActionController::Base
   end
 
   def current_user
-    @current_user = User.find(session[:user_id]) if session[:user_id]
-    #why add(if session[:user_id])
+    @current_user ||= session[:user_id] && User.find(session[:user_id])
+
   end
 end
